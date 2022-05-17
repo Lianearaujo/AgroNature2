cfd75f5be0b241e3df590839814e198da0a4d118
Liane <100138807+Lianearaujo@users.noreply.github.com>
2022-05-17 14:31:52 -0300

    3.4

--- database/database.js
+++ database/database.js
@@ -5,5 +5,12 @@ const connection = new Sequelize('cadastro', 'root', '123456789', {
   dialect: 'mysql',
   port: 3306
 });
-
+Sequelize
+        .authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.');
+        })
+        .catch((err) => {
+            console.log('Unable to connect to the database:', err);
+        });
 module.exports = connection;
