c84f354213cb0e52646e3b7cbd96f87ae9477f07
Liane <100138807+Lianearaujo@users.noreply.github.com>
2022-05-17 13:54:52 -0300

    2.1

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('AgroNature', 'p101332359097-94p8te@gcp-sa-cloud-sql.iam.gserviceaccount.com', '123456789', {
-  host: '10.90.192.4',
+const connection = new Sequelize('AgroNature', 'root', '123456789', {
+  host: '34.82.206.27',
   dialect: 'mysql'
 });
 
