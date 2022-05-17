6584049519f1121e489e7ebd5693718689339458
Liane <100138807+Lianearaujo@users.noreply.github.com>
2022-05-17 12:32:48 -0300

    2.0

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('AgroNature', 'root', 'bonita007', {
-  host: 'localhost',
+const connection = new Sequelize('AgroNature', 'rootadmin@agriculaliane', 'Liane@araujo2128', {
+  host: 'agriculaliane.mysql.database.azure.com',
   dialect: 'mysql'
 });
 
