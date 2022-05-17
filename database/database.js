11762bd0d9783a34929abcfefc23528ccaa4fb58
Liane <100138807+Lianearaujo@users.noreply.github.com>
2022-05-17 13:16:24 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('AgroNature', 'rootadmin@agriculaliane', 'Liane@araujo2128', {
-  host: 'agriculaliane.mysql.database.azure.com',
+const connection = new Sequelize('AgroNature', 'p101332359097-94p8te@gcp-sa-cloud-sql.iam.gserviceaccount.com', '123456789', {
+  host: '10.90.192.4',
   dialect: 'mysql'
 });
 
