618ca8677b706db6f9a3ca4a5ad4c3fe581e18f3
Liane <100138807+Lianearaujo@users.noreply.github.com>
2022-05-17 14:30:09 -0300

    3.3

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize('cadastro', 'root', '123456789', {
   host: '34.82.206.27',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  port: 3306
 });
 
 module.exports = connection;
